2cadd8e1eba798fcf42b1daaf386ae1268ee65c6
tshres <tshres@uw.edu>
2023-04-25 14:45:46 -0700

    complete

---
+++ first_normal_form.sql
@@ -0,0 +1,23 @@
+CREATE TABLE temp_table AS
+SELECT abilities, pokedex_number
+FROM imported_pokemon_data;
+
+CREATE TABLE pokemon AS
+SELECT abilities, trim(value) AS ability, pokedex_number
+FROM temp_table, json_each('["' || replace(abilities, ',', '","') || '"]')
+WHERE ability <> ' ';
+
+CREATE TABLE pokemon_ability AS
+SELECT ability, pokedex_number
+FROM pokemon;
+DROP TABLE pokemon;
+DROP TABLE temp_table;
+
+UPDATE pokemon_ability SET ability = REPLACE(ability, '[', '');
+UPDATE pokemon_ability SET ability = REPLACE(ability, '''', '');
+UPDATE pokemon_ability SET ability = REPLACE(ability, ']', '');
+
+
+CREATE TABLE pokemon AS
+SELECT against_bug, against_dark, against_dragon, against_electric, against_fairy, against_fight, against_fire, against_flying, against_ghost, against_grass, against_ground, against_ice, against_normal, against_poison, against_psychic, against_rock, against_steel, against_water, attack, base_egg_steps, base_happiness, base_total, capture_rate, classfication, defense, experience_growth, height_m, hp, percentage_male, pokedex_number, sp_attack, sp_defense, speed, type1, type2, weight_kg, generation, is_legendary
+FROM imported_pokemon_data;
